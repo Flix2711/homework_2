96878bbea65653db8b38b10c6b1b851987936b22
Appolia <VasyGrenkin@gmail.com>
2022-07-17 20:37:54 +0300

    ex_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // ex 1
         var box = 1;
         byte a = 13;
         short b = 313;
@@ -7,5 +8,14 @@ public class Main {
         long d = 131313131313131L;
         float e = 13.13f;
         double f = 13.13131313;
+
+        //  ex2
+        float boxer1 = 78.2f;
+        float boxer2 = 82.7f;
+        float boxersWeight = boxer1 + boxer2;
+        System.out.println("Общий вес боксеров " + boxersWeight);
+        float diffBoxersWeight = boxer2 - boxer1;
+        System.out.println("Разница в весе боксеров " + diffBoxersWeight);
+
     }
 }
