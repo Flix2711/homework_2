5dd1a482edcc9afc9007ccf79f9769eece6d02dd
Appolia <VasyGrenkin@gmail.com>
2022-07-17 20:12:54 +0300

    ex_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var box = 1;
+        byte a = 13;
+        short b = 313;
+        int c = 131313;
+        long d = 131313131313131L;
+        float e = 13.13f;
+        double f = 13.13131313;
     }
 }
